079b01051719c21da95ef4236e71b586c289e99a
UA_Girl <n_kholodkova@ukr.net>
2021-05-09 23:41:44 +0300

    level 1 and level 80. Passed tests

--- src/transform.js
+++ src/transform.js
@@ -1,5 +1,5 @@
 // assign 80 once you accept a challenge to implement Level 80 requirements
-export const level = 1;
+export const level = 80;
 
 /*
   Add parameters as appropriate.
@@ -7,7 +7,55 @@ export const level = 1;
   Should work with arbitrary function expressions passed.
   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
 */
-export function transform() {}
+
+function convertNumber(num) {
+  return num * 2 + 3;
+}
+
+function convertToLovercase(str) {
+  return str.toLowerCase();
+}
+
+function convertFuncName(func, arg, val) {
+  return func(arg, val);
+}
+
+function convertToJSON(obj) {
+  return JSON.stringify(obj);
+}
+
+function findHandler(arg) {
+  switch (typeof arg) {
+    case 'number':
+      return convertNumber;
+    case 'string':
+      return convertToLovercase;
+    case 'function':
+      return convertFuncName;
+    default:
+      return convertToJSON;
+  }
+}
+
+export function transform(strings, ...values) {
+  let parsedString = ``;
+  let i = 0;
+  while (i < values.length) {
+    if (typeof values[i] === 'function') {
+      if (values[i + 1]) {
+        parsedString += `${strings[i]}${convertFuncName(values[i], strings[i + 1], values[i + 1])}`;
+        i += 2;
+      } else {
+        parsedString += `${strings[i]}${convertFuncName(values[i])}`;
+        i += 1;
+      }
+    } else {
+      parsedString += `${strings[i]}${findHandler(values[i])(values[i])}`;
+      i += 1;
+    }
+  }
+  return parsedString;
+}
 
 /*
   Level 1: expects no parameters. Returns its own name capitalized.
@@ -19,4 +67,18 @@ export function transform() {}
     Usage example:
       testFunction("some Text", 125) should return 'TESTFUNCTION(someText=125)'
  */
-export function testFunction() {}
+
+// LEVEL 1
+
+// export function testFunction() {
+//   return testFunction.name.toUpperCase();
+// }
+
+// LEVEL 2
+
+export function testFunction(argName, valName) {
+  if (valName && argName) {
+    return `${testFunction.name.toUpperCase()}(${argName.replace(/\s/g, '')}=${valName})`;
+  }
+  return `${testFunction.name.toUpperCase()}`;
+}
